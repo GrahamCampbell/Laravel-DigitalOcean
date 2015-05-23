86b5aea5948da40b3e7fc7dd2edc570b17962e9d
Graham Campbell <graham@cachethq.io>
2015-05-23 15:02:30 +0100

    Updated my email

--- src/DigitalOceanManager.php
+++ src/DigitalOceanManager.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of Laravel DigitalOcean.
  *
- * (c) Graham Campbell <graham@mineuk.com>
+ * (c) Graham Campbell <graham@cachethq.io>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -28,7 +28,7 @@ use Illuminate\Contracts\Config\Repository;
  * @method \DigitalOceanV2\Api\Droplet droplet()
  * @method \DigitalOceanV2\Api\RateLimit rateLimit()
  *
- * @author Graham Campbell <graham@mineuk.com>
+ * @author Graham Campbell <graham@cachethq.io>
  */
 class DigitalOceanManager extends AbstractManager
 {
