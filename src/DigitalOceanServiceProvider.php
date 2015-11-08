d063965bd1e1a1aa80f09b1fd78f1ec33c9f2bcf
Graham Campbell <graham@alt-three.com>
2015-11-08 19:56:42 +0000

    Improved environment specific code

--- src/DigitalOceanServiceProvider.php
+++ src/DigitalOceanServiceProvider.php
@@ -14,7 +14,9 @@ namespace GrahamCampbell\DigitalOcean;
 use DigitalOceanV2\DigitalOceanV2;
 use GrahamCampbell\DigitalOcean\Adapters\ConnectionFactory as AdapterFactory;
 use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Foundation\Application as LaravelApplication;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Lumen\Application as LumenApplication;
 
 /**
  * This is the digitalocean service provider class.
@@ -44,9 +46,9 @@ class DigitalOceanServiceProvider extends ServiceProvider
     {
         $source = realpath(__DIR__.'/../config/digitalocean.php');
 
-        if (class_exists('Illuminate\Foundation\Application', false) && $app->runningInConsole()) {
+        if ($app instanceof LaravelApplication && $app->runningInConsole()) {
             $this->publishes([$source => config_path('digitalocean.php')]);
-        } elseif (class_exists('Laravel\Lumen\Application', false)) {
+        } elseif ($app instanceof LumenApplication) {
             $app->configure('digitalocean');
         }
 
